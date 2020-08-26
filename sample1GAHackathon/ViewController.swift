c267957719d85d191f6ba90b78084e7615742c3e
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-08-26 20:30:17 +0900

    とりあえず通知が反映されるまで

--- sample1GAHackathon/ViewController.swift
+++ sample1GAHackathon/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+             
+             let content = UNMutableNotificationContent()
+             content.title = "テスト"
+             content.body = "通知"
+             content.sound = UNNotificationSound.default
+             
+             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+             let request = UNNotificationRequest(identifier: "hoge", content: content, trigger: trigger)
+             let center = UNUserNotificationCenter.current()
+             center.add(request)
+             print("通知完了")
+    }
+    
 }
 
