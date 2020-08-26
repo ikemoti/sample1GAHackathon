541068c957d7c857735afb16b148b20f7ec5cbfc
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-08-26 18:29:44 +0900

    Initial Commit

---
+++ sample1GAHackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample1GAHackathon
+//
+//  Created by Sousuke Ikemoto on 2020/08/26.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sample1GAHackathonTests/sample1GAHackathonTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample1GAHackathonTests.swift
+//  sample1GAHackathonTests
+//
+//  Created by Sousuke Ikemoto on 2020/08/26.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import XCTest
+@testable import sample1GAHackathon
+
+class sample1GAHackathonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
